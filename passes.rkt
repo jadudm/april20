4efc644f734a3a207b9ad59c35d2846ff509ee0f
nelliott <ndelliott@gmail.com>
2011-02-26 15:17:39 -0500

    more

--- passes.rkt
+++ passes.rkt
@@ -8,6 +8,7 @@
              (handleop op) (ast-to-llvm lhs) (ast-to-llvm rhs))]
     [(assign name val) (format "%~a = ~a" name (ast-to-llvm val))]
     [(result name) (format "%result = ~a" name)] 
+    [(program statements) (map ast-to-llvm statements)]
     ))
     
 (define (handleop op)
