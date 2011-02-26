631a0f8c60c65249a1a1c1ad46bdecc3b86ce73d
nelliott <ndelliott@gmail.com>
2011-02-26 15:20:56 -0500

    corrected errors in result handling in passes.rkt

--- passes.rkt
+++ passes.rkt
@@ -8,7 +8,7 @@
     [(binop op lhs rhs) (format "~a i32 ~a, ~a" 
              (handleop op) (ast-to-llvm lhs) (ast-to-llvm rhs))]
     [(assign name val) (format "%~a = ~a" name (ast-to-llvm val))]
-    [(result name) (format "%result = ~a" name)] 
+    [(result name) (format "%result = %~a" name)] 
     [(program statements) (map ast-to-llvm statements)]
     ))
     
